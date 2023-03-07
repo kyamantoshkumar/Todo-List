c049369c8fd4ceb0c46b36963b0c8aac67a1af19
Mantosh <kyamantoshkumar@gmail.com>
2023-03-07 07:37:06 +0530

    Create Newfile.js

---
+++ Newfile.js
@@ -0,0 +1,37 @@
+
+ const Login = () => { 
+             <form class>
+               <table>
+                <tr>
+                <label class="form-control">User Email</label>
+                <input type="email" 
+                    name="name"
+                    id="usename"
+                    placeholder="Enter User Name"
+                />
+                </tr>
+                <tr>
+                <label class="form-control">Password</label>
+                <input type="password" 
+                    name="password"
+                    id="usename"
+                    placeholder="Enter User Name"
+                /> 
+                </tr>
+               </table>
+               <button class="btn"> submit</button>
+             </form>
+ }
+
+// var a = parseInt(Number("Enter a number"));
+// var b = parseInt(Number("Enter b number"));
+
+
+
+ 
+
+
+
+
+
+
